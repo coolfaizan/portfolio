184e53f076cd4bec92b5c9652620a2a48809ccfd
coolfaizan <mdfaizanmallick000786@gmail.com>
2026-07-29 23:07:32 +0530

    basic portfolio

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// let links = document.querySelectorAll(".nav-link");
+// let current = null;
+
+// links.forEach((link) => {
+//     link.addEventListener("click", function () {
+
+//         if (current) {
+//             current.classList.add("vapour"); // Hide previous section
+//         }
+
+//         let div = document.querySelector("." + this.id);
+
+//         div.classList.remove("vapour"); // Show current section. vapour mtlb display none;
+//         current = div;
+//     });
+// });
+
+
+let links = document.querySelectorAll(".nav-link");
+let sections=document.querySelectorAll(".about, .education, .skills, .project, .contact");
+links.forEach((link) => {
+    link.addEventListener("click", function () {
+        sections.forEach((section)=>{
+            section.classList.add("vapour");
+        })
+        let div = document.querySelector("." + this.id);
+        div.classList.remove("vapour"); // Show current section. vapour mtlb display none;
+    });
+});
+let btn=document.querySelector("#submit");
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let inp=document.querySelectorAll(".form-control");
+    for (i of inp){
+        i.value="";
+    }
+});
+
